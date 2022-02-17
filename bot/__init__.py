4e8bbd11c907210e032146c7b8b2826770d9a004
Kaif <88398455+kaif-00z@users.noreply.github.com>
2022-02-17 18:29:01 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -6,7 +6,7 @@ import sys
 import time
 import traceback
 
-import youtube_dl
+from yt_dlp import YoutubeDL
 from telethon import Button, TelegramClient, errors, events
 from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
 
