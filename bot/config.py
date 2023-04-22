f377682272c103b653b054e3c35020ff27a28251
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-04-23 01:11:23 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,8 +4,8 @@ from decouple import config
 
 try:
     API_ID = config("API_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    API_HASH = config("API_HASH","12bbd720f4097ba7713c5e40a11dfd2a")
+    BOT_TOKEN = config("BOT_TOKEN","6020053428:AAGaKdIalVe2UDk1PDzFc5Rz5E_bBfF6Dw8")
     OWNER = config("OWNER_ID", cast=int)
 
 except Exception as e:
