f4b5690cfede06d34c2952dc0249786cc575c936
Kunal <113813635+kunal9322@users.noreply.github.com>
2023-04-23 01:12:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,10 +3,10 @@
 from decouple import config
 
 try:
-    API_ID = config("API_ID", cast=int)
+    API_ID = config("API_ID", "16743442")
     API_HASH = config("API_HASH","12bbd720f4097ba7713c5e40a11dfd2a")
     BOT_TOKEN = config("BOT_TOKEN","6020053428:AAGaKdIalVe2UDk1PDzFc5Rz5E_bBfF6Dw8")
-    OWNER = config("OWNER_ID", cast=int)
+    OWNER = config("OWNER_ID", "5885920877")
 
 except Exception as e:
     print("Environment vars Missing")
